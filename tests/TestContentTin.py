8694641ee4376b2257b6b5c6becdbef2ea45ba54
Thomas Fenzl <thomas.fenzl@tngtech.com>
2011-09-09 17:44:57 +0200

    refactored common test setup

--- tests/TestContentTin.py
+++ tests/TestContentTin.py
@@ -7,34 +7,32 @@ from svnfiltereddump import ContentTin
 
 class ContentTinTests(TestCase):
 
+    def setUp(self):
+        self.fh = StringIO("xxxYYYzzz")
+        self.out_fh = StringIO()
+
     def test_simple_tin(self):
-        fh = StringIO("xxxYYYzzz")
-        fh.read(3)
-        out_fh = StringIO()
+        self.fh.read(3)
 
-        with ContentTin(fh, 3) as tin:
-            tin.empty_to(out_fh)
+        with ContentTin(self.fh, 3) as tin:
+            tin.empty_to(self.out_fh)
 
-        out_fh.seek(0)
-        self.assertEqual(out_fh.read(), 'YYY')
+        self.out_fh.seek(0)
+        self.assertEqual(self.out_fh.read(), 'YYY')
   
     def test_discard_tin(self):
-        fh = StringIO("xxxYYYzzz")
-        with ContentTin(fh, 3) as ct:
+        with ContentTin(self.fh, 3) as ct:
             pass
 
-        self.assertEqual(fh.tell(), 3)
+        self.assertEqual(self.fh.tell(), 3)
 
 
     def test_throws_on_overrun(self):
         fh = StringIO("")
         tin = ContentTin(fh, 3)
-        out_fh = StringIO()
-        self.assertRaises(Exception, tin.empty_to, out_fh)
+        self.assertRaises(Exception, tin.empty_to, self.out_fh)
 
     def test_only_usable_once(self):
-        fh = StringIO("asdfasdfasd")
-        out_fh = StringIO()
-        with ContentTin(fh, 4) as tin:
-            tin.empty_to(out_fh)
-            self.assertRaises(Exception, tin.empty_to, out_fh)
+        with ContentTin(self.fh, 4) as tin:
+            tin.empty_to(self.out_fh)
+            self.assertRaises(Exception, tin.empty_to, self.out_fh)
